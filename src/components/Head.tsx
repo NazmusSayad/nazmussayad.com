2b2e45543bc8d91f00892106aac0723a4ceaebea
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-03-12 10:49:56 +0000

    font

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -46,6 +46,11 @@ const Head = () => {
 
       <meta name="google" content="nositelinkssearchbox" />
       <meta name="googlebot" content="translate" />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
     </HeadWrapper>
   )
 }
